28c32064b21e45324e4c12b1e6beda2fca02676d
Shirish-Bashyal <104261483+Shirish-Bashyal@users.noreply.github.com>
2025-09-09 16:15:13 +0545

    HelloWorld First Assingment

---
+++ Console/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter your name:");
+var name = Console.ReadLine();
+Console.Clear();
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to c# programming!");
+Console.WriteLine($"My name is {name}");
+Console.Write("Todays Date:- ");
+Console.WriteLine(DateOnly.FromDateTime(DateTime.Now));
