0c64e773ede4d4c6bc1c9c54ec2759a6df347215
tamu <delltamu0@gmail.com>
2025-09-10 08:41:52 +0545

    real one

---
+++ intern/internship-batch-7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace internship_batch_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World !");
+            Console.WriteLine("Welcome to c# Programming !");
+            Console.WriteLine("My name is Nirajan");
+
+            Console.WriteLine();
+
+            // 100 words reflection on me
+            Console.WriteLine("Refection on me");
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Programming to me, is both a creative and logical process that allows me to solve real-world problems by transforming ideas into functional solutions. Through programming, I can continuously grow and challenge myself, which is a key motivator. For this internship, my goal is to deepen my understanding of C# and strengthen my coding skills. I also hope to learn about microservices, .NET, Angular, and stored procedures. Gaining expertise in these technologies will expand my ability to design scalable, efficient applications and give me a comprehensive understanding of modern software development practices. This opportunity is a stepping stone towards becoming a well-rounded developer.");
+
+            Console.WriteLine();
+
+            //Bonus challenge
+            Console.WriteLine("Bonus Challenge");
+            Console.WriteLine("-----------------");
+            Console.Write("Enter your User name : ");
+            string username = Console.ReadLine();
+            Console.WriteLine("Your's Username is " + username);
+
+            string date = DateTime.Now.ToString("yyyy-MM-dd"); 
+            Console.WriteLine("Today Date is : " + date);
+
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ intern/internship-batch-7/Student_crud.cs
@@ -0,0 +1,128 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace internship_batch_7
+{
+   class Student_crud
+    {
+        private void Main(string[] args)
+        {
+            SqlConnection cn = new SqlConnection("Data Source=DESKTOP-OFDB3MH;Initial Catalog=amnil;Persist Security Info=True;User ID=nirajan;Password=nir@j@n17");
+
+			try
+			{
+				cn.Open();
+
+                //declare 
+                int id;
+                string fName;
+                string lName;
+                string gender;
+                string roll;
+                string grade;
+
+                //list
+                string list = "select * from tbl_student";
+                SqlCommand cmdList = new SqlCommand(list, cn);
+                SqlDataReader dr = cmdList.ExecuteReader();
+                var table = new Table();
+
+                table.AddColumn("Id");
+                table.AddColumn("First Name");
+                table.AddColumn("Last Name");
+                table.AddColumn("Gender");
+                table.AddColumn("RollNumber");
+                table.AddColumn("Grade");
+
+                while (dr.Read())
+                {
+                    table.AddRow(
+                        dr["Id"].ToString(),
+                        dr["FirstName"].ToString(),
+                        dr["LastName"].ToString(),
+                        dr["Gender"].ToString(),
+                        dr["RollNumber"].ToString(),
+                        dr["Grade"].ToString()
+                    );
+                }
+                dr.Close();
+                AnsiConsole.Write(table);
+
+                //insert
+                Console.WriteLine();
+                Console.WriteLine("Add student");
+                Console.WriteLine("---------------");
+                Console.Write("Enter your First Name : ");
+                fName = Console.ReadLine();
+
+                Console.Write("Enter your Last Name : ");
+                lName = Console.ReadLine();
+
+                Console.Write("Enter your Gender : ");
+                gender = Console.ReadLine();
+
+                Console.Write("Enter your Roll Number : ");
+                roll = Console.ReadLine();
+
+                Console.Write("Enter your Grade : ");
+                grade = Console.ReadLine();
+
+                //sql query
+                string insert = "Insert into tbl_student(firstName,lastName,gender,rollNumber,grade) Values('" + fName + "','" + lName + "','" + gender + "','" + roll + "','" + grade + "')";
+                SqlCommand cmdInsert = new SqlCommand(insert, cn);
+                cmdInsert.ExecuteNonQuery();
+                Console.WriteLine("Insert successfully !");
+
+                // edit
+                Console.WriteLine();
+                Console.WriteLine("update student");
+                Console.WriteLine("---------------");
+
+                Console.Write("Enter Id of student you want to update : ");
+                id = int.Parse(Console.ReadLine());                
+
+                //update 
+                Console.Write("Enter your First Name : ");
+                fName = Console.ReadLine();
+
+                Console.Write("Enter your Last Name : ");
+                lName = Console.ReadLine();
+
+                Console.Write("Enter your Gender : " );
+                gender = Console.ReadLine();
+
+                Console.Write("Enter your Roll Number : ");
+                roll = Console.ReadLine();
+
+                Console.Write("Enter your Grade : ");
+                grade = Console.ReadLine();
+
+                string update = "update tbl_student set firstName= '" + fName + "', lastName= '" + lName + "', gender= '" + gender + "', rollNumber= '" + roll + "', grade= '" + grade + "' where id='" + id + "' ";
+                SqlCommand cmdUpdate = new SqlCommand(update, cn);
+                cmdUpdate.ExecuteNonQuery();
+                Console.WriteLine("Update successfully");
+
+                //delete
+                Console.Write("Enter Id of student you want to Delete : ");
+                id = int.Parse(Console.ReadLine());
+                string delete = "delete from tbl_student where id= '" + id + "' ";
+                SqlCommand cmdDelete = new SqlCommand(delete, cn);
+                cmdDelete.ExecuteNonQuery();
+                Console.WriteLine("Delete successfully !");
+
+                cn.Close();
+            }
+			catch (Exception)
+			{
+
+				throw;
+			}
+        }
+    }
+}
