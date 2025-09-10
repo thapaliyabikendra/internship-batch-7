c1b7dd63f1afc759fa9be6c3f97a98afb462ebe5
Shirish-Bashyal <104261483+Shirish-Bashyal@users.noreply.github.com>
2025-09-10 11:36:43 +0545

    complete StudentCrud console application

---
+++ Console/StudentCrud/Program.cs
@@ -0,0 +1,76 @@
+﻿using StudentCrudConsole;
+
+try
+{
+    StudentCrud StudentManager = new StudentCrud();
+    while (true)
+    {
+        Console.WriteLine("enter 1,2,3,4 to create, read, update, delete students data");
+        var userInput = Console.ReadLine();
+
+        switch (userInput)
+        {
+            case "1":
+                Console.WriteLine("Enter id, name and address for student with a space ");
+                var inputData = Console.ReadLine().Split(" ");
+                var stdData = new StudentModel()
+                {
+                    Id = Convert.ToInt32(inputData[0]),
+                    Name = inputData[1],
+                    Address = inputData[2]
+                };
+
+                var result = StudentManager.AddStudent(stdData);
+                Console.WriteLine(result);
+                break;
+
+            case "2":
+                Console.WriteLine("The students information is:");
+                var students = StudentManager.GetStudents();
+                foreach (var std in students)
+                {
+                    Console.WriteLine($"Name= {std.Name}, Address= {std.Address}");
+                }
+                break;
+
+            case "3":
+                Console.WriteLine("Provide the student id and both name and address to be updated");
+                var inputUpdateData = Console.ReadLine().Split(" ");
+                var stdUpdateData = new StudentModel()
+                {
+                    Id = Convert.ToInt32(inputUpdateData[0]),
+                    Name = inputUpdateData[1],
+                    Address = inputUpdateData[2]
+                };
+                var updateResult = StudentManager.UpdateStudent(stdUpdateData);
+                Console.WriteLine(updateResult);
+                break;
+
+            case "4":
+                Console.WriteLine("Provide the id of the student to delete");
+                var studentId = Convert.ToInt32(Console.ReadLine());
+                var deleteResult = StudentManager.DeleteStudent(studentId);
+
+                Console.WriteLine(deleteResult);
+                break;
+
+            default:
+                Console.WriteLine("Invalid option. Please enter 1, 2, 3, or 4.");
+                break;
+        }
+        Console.WriteLine("Do you want to continue: Y for yes ");
+        var userentry = Console.ReadLine();
+        if (userentry == "Y")
+            continue;
+        else
+        {
+            Console.WriteLine("Program ended ");
+
+            break;
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
---
+++ Console/StudentCrud/StudentCrud.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentCrudConsole
+{
+    /// <summary>
+    ///represents a student entity
+    /// </summary>
+    public class StudentModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+
+    /// <summary>
+    /// performs CRUD operations on student entity
+    /// </summary>
+    public class StudentCrud
+    {
+        private Dictionary<int, StudentModel> StudentCache = new Dictionary<int, StudentModel>();
+
+        /// <summary>
+        /// Adds a new student object to the memory
+        /// </summary>
+        /// <param name="model"> represents student object </param>
+        /// <returns> message if the operation is success or not</returns>
+        public string AddStudent(StudentModel model)
+        {
+            if (StudentCache.TryAdd(model.Id, model))
+                return "Student Added";
+
+            return "Id already exists";
+        }
+
+        /// <summary>
+        /// Gets all the students from the memory
+        /// </summary>
+        /// <returns> list of student entity if any exists</returns>
+        public ICollection<StudentModel> GetStudents()
+        {
+            return StudentCache.Values;
+        }
+
+        /// <summary>
+        /// updates the existing student entity
+        /// </summary>
+        /// <param name="model"> represent student object</param>
+        /// <returns> message if the operation is success or not </returns>
+        public string UpdateStudent(StudentModel model)
+        {
+            if (StudentCache.ContainsKey(model.Id))
+            {
+                StudentCache[model.Id] = model;
+                return "Student updated";
+            }
+            else
+                return "Student doesnot exists";
+        }
+
+        /// <summary>
+        /// deletes the student entity on the basis of id
+        /// </summary>
+        /// <param name="id"> represents student id</param>
+        /// <returns>message if the operation is success or not</returns>
+        public string DeleteStudent(int id)
+        {
+            if (StudentCache.ContainsKey(id))
+            {
+                StudentCache.Remove(id);
+                return "Student Deleted";
+            }
+            else
+                return "Student not found";
+        }
+    }
+}
