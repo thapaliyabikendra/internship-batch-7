bef8c8d1a64f0f612c0c40d758f4665360bc5a29
tamu <delltamu0@gmail.com>
2025-09-16 13:35:01 +0545

    feat[assignment_five]: zoo management system
     Task:
      - Uses proper encapsulation with validation
      - Implements 3-level inheritance hierarchy
      - Demonstrates polymorphism with virtual/override
      - Create at least 4 different animal types
    
     Bonus challenge:
     	- new class name IFeedable
    
     # all assignment and bonus challenges are in assignment_five folder

---
+++ assignment_five/assignment_five/IFeedable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace assignment_five
+{
+    internal interface IFeedable
+    {
+        string _name { get; set; }
+        string GetFeedingSchedules();
+        void Feed();
+    }
+
+    //  interface Ifeedable implementing (from all of the above methods, property should implement while inheritance)
+    public class Cow : IFeedable
+    { 
+        // property 
+        public string _name { get; set; }
+
+        public string GetFeedingSchedules()
+        {
+            return "Cow eat twice times a day.";
+        }
+        public void Feed()
+        {
+            Console.WriteLine("Cow eat grass.");
+        }
+
+    }
+
+    public class Goat : IFeedable
+    { 
+        public string _name { get; set; }
+
+        public string GetFeedingSchedules()
+        {
+            return "Goat eat three times a day.";
+        }
+        public void Feed()
+        {
+            Console.WriteLine("Goat also eat grass hay.");
+        }
+    }
+}
---
+++ assignment_five/assignment_five/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace assignment_five
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zoo Management System");
+            Console.WriteLine("------------------------");
+
+
+            // create instance of derived class
+            Animal animal = new Animal();
+            Dog dog = new Dog();
+            Eagle eagle = new Eagle();
+            GoldFish fish = new GoldFish();
+
+            // call the method on the derived class objects
+            dog.MammalsName();
+            double dogCost = dog.CalculateFoodCost();
+            Console.WriteLine("The cost is : " +dogCost);
+
+            Console.WriteLine();
+            eagle.BirdName();
+            double birdCost= eagle.CalculateFoodCost();
+            Console.WriteLine("The cost is : " +birdCost);
+
+            Console.WriteLine();
+            fish.FishName();
+            double fishCost= fish.CalculateFoodCost();
+            Console.WriteLine("The cost is : " +fishCost);
+
+            // Bonus challenges
+            Console.WriteLine();
+            Console.WriteLine("------------------");
+            Console.WriteLine("Bonus Challenge");
+            Console.WriteLine("__________________");
+
+            Console.Write("Enter Cow name : ");
+            string cowName= Console.ReadLine();
+
+            Console.WriteLine();
+            Console.Write("Enter Goat name : ");
+            string goatName = Console.ReadLine();
+            Console.WriteLine();
+
+            Cow cow = new Cow { _name = cowName };
+            Goat goat = new Goat { _name = goatName };
+
+            List<IFeedable> animals = new List<IFeedable>();
+            animals.Add(cow);
+            animals.Add(goat);
+
+            foreach (var animalName in animals)
+            {
+                Console.WriteLine($"Animal name : {animalName._name}");
+                Console.WriteLine($"Feeding Schedule :{ animalName.GetFeedingSchedules()}");
+                animalName.Feed();
+
+                Console.WriteLine() ;
+            }
+
+        }
+
+        #region Q1) Implement Encapsulation :
+
+        // private field
+        private  int _id;
+
+        // public read only property for _id
+        public int id { get { return _id; }}
+
+        // validate age (0-100) in property set
+        private int _age;
+        public int age { get { return _age; } set { _age = (value > 0 || value < 100) ? value  : throw new ArgumentOutOfRangeException("Please insert age between 0 to 100") ; } }
+
+
+        #endregion
+
+        #region Q2) Add Inheritance Hierarchy:
+
+        // base class Animal
+        private class Animal
+        {
+            //property 
+
+            // this is for Q3) virtual method
+
+            public virtual double CalculateFoodCost()
+            {
+                return 0;
+            }
+        }
+
+        // derived class inheritance in Reptile from Animal
+        private class Reptile : Animal
+        {
+
+        }
+
+        // derived class inheritance in Snake from Raptile
+        private class Snake : Reptile
+        {
+
+        }
+
+        // derived class inheritance in Turtle from Reptile
+        private class Turtle : Reptile
+        {
+
+        }
+
+        #endregion
+
+        #region Q3) Demonstrate Polymorphism:
+
+        //Animal base class is already in Q2.
+
+        // Type of Animals
+        private class Mammals : Animal
+        {
+            public virtual void MammalsName()
+            {
+                Console.WriteLine("Name of Mammals ");
+            }
+        }
+        private class Birds : Animal
+        {
+            public virtual void BirdName()
+            {
+                Console.WriteLine("Name of Birds ");
+            }
+        }
+        private class Fish : Animal
+        {
+            public virtual void FishName()
+            {
+                Console.WriteLine("Name of Fish ");
+            }
+        }
+
+        // derived class of type of Animals
+        private class Dog : Mammals
+        {
+            public override void MammalsName()
+            {
+                Console.WriteLine("Mammal Name of dog: german shepherd");
+            }
+            public override double CalculateFoodCost()
+            {
+                return 2000;
+            }
+        }
+
+        private class Eagle : Birds
+        {
+            public override void BirdName()
+            {
+                Console.WriteLine("Bird Name : eagle");
+            }
+            public override double CalculateFoodCost()
+            {
+                return 300;
+            }
+        }
+
+        private class GoldFish : Fish
+        {
+            public override void FishName()
+            {
+                Console.WriteLine("Fish Name : goldfish");
+            }
+            public override double CalculateFoodCost()
+            {
+                return 150;
+            }
+        }
+
+        #endregion
+
+    }
+}
