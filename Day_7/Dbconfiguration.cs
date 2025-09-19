d2cebb78f5ba361de35c926202995753ff3583ec
Subesh11 <125328426+Subesh11@users.noreply.github.com>
2025-09-19 14:14:43 +0545

    feat(Student)!: implement ORM

---
+++ Day_7/Dbconfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Day_7;
+public class Student
+{
+    public int StudentId { get; set; }
+    public string ? Name { get; set; }
+    public int Age { get; set; }
+}
+public class Dbconfiguration:DbContext
+{
+    public DbSet<Student> Student { get; set; }
+    public string Dbpath { get; }
+    public Dbconfiguration()
+    {
+        Dbpath = Dbpath = Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\student.db"); ;
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite($"Data Source={Dbpath}");
+    
+}
---
+++ Day_7/Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+using Day_7;
+
+using var db = new Dbconfiguration();
+Console.WriteLine($"Using DB at: {Path.GetFullPath(db.Dbpath)}");
+
+db.Student.Add(new Student() {Name ="Subesh",Age=23} );
+db.SaveChanges();
+Console.WriteLine("1");
+Console.WriteLine("Initial value");
+foreach (var s in db.Student.ToList())
+{
+    Console.WriteLine($"ID: {s.StudentId}, Name: {s.Name}, Age: {s.Age}\n");
+}
+
+
+var students =db.Student.Where(s=>s.Age>18).ToList();
+Console.WriteLine("2");
+foreach (var s in db.Student.ToList())
+{
+    Console.WriteLine($"ID: {s.StudentId}, Name: {s.Name}, Age: {s.Age}\n");
+}
+
+
+var student = db.Student.FirstOrDefault(s => s.Name == "Subesh");
+if (student == null)
+    throw new Exception("Their is no value");
+student.Name = "Laxman";
+Console.WriteLine("3");
+db.SaveChanges();
+foreach (var s in db.Student.ToList())
+{
+    Console.WriteLine($"ID: {s.StudentId}, Name: {s.Name}, Age: {s.Age}\n");
+}
+
+
+var student2 = db.Student.FirstOrDefault(s => s.Name == "Laxman");
+Console.WriteLine("4");
+db.SaveChanges();
+if (student2 == null)
+{
+    throw new Exception("No Student with name laxman\n");
+}
+db.Student.Remove(student2);
+
+db.SaveChanges();
+Console.WriteLine("5");
+foreach (var s in db.Student.ToList())
+{
+    Console.WriteLine($"ID: {s.StudentId}, Name: {s.Name}, Age: {s.Age} \n");
+}
+
