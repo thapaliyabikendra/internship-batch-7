fce943b571f097584bfb217f7bd3b74d8d7dcc9d
Shirish-Bashyal <104261483+Shirish-Bashyal@users.noreply.github.com>
2025-10-07 15:56:45 +0545

    feat: MVC project added

---
+++ AttendanceManagementSystem/AttendanceManagementSystem.Web/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using AttendanceManagementSystem.Shared.Dtos;
+using AttendanceManagementSystem.Shared.Dtos.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AttendanceManagementSystem.Web.Controllers;
+
+public class UserController : Controller
+{
+    private readonly IHttpClientFactory _clientFactory;
+
+    public UserController(IHttpClientFactory clientFactory)
+    {
+        _clientFactory = clientFactory;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var client = _clientFactory.CreateClient("AttendanceApi");
+        var response = await client.GetAsync("User/all");
+        if (!response.IsSuccessStatusCode)
+        {
+            ViewBag.Error = "Unable to fetch users.";
+            return View();
+        }
+        var result = await response.Content.ReadFromJsonAsync<
+            ServiceResponseDto<IEnumerable<UserDto>>
+        >();
+
+        if (result == null || !result.IsSuccess || result.Data == null)
+        {
+            ViewBag.Error = result?.Message;
+            return View();
+        }
+
+        return View(result.Data);
+    }
+}
---
+++ AttendanceManagementSystem/AttendanceManagementSystem.Web/Models/UserModel.cs
@@ -0,0 +1,7 @@
+﻿namespace AttendanceManagementSystem.Web.Models;
+
+public class UserModel
+{
+    public string Name { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+}
